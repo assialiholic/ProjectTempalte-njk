cbc6841ccaf56df6bd76f64569473f4d5819638f
Atsushi Handa <a.handa@24-7.co.jp>
2017-11-17 15:42:40 +0900

    不要タスク削除

--- gulpfile.js
+++ gulpfile.js
@@ -6,46 +6,11 @@ const gulp = require('gulp'),
     p = require('gulp-load-plugins')();
 
 const SRC = 'src',
-    DEST = 'public_html',
-    GUIDE = 'style_guide';
+    DEST = 'public_html';
 
 function plumberNotify(){
   return p.plumber({errorHandler: p.notify.onError("<%= error.message %>")});
 }
-
-// -------------------------------------------------------------------
-// HTML
-// -------------------------------------------------------------------
-
-//nunjucksのコンパイル
-gulp.task('njk', function(){
-  return gulp.src([
-    SRC + '/**/*.njk',
-    '!src/_inc/*.njk'
-  ])
-  .pipe(p.cached())
-  .pipe(p.using())
-  .pipe(plumberNotify())
-  .pipe(p.nunjucksRender({path:SRC +'/'}))
-  .pipe(gulp.dest(DEST))
-});
-
-
-//ソースの品質チェック
-gulp.task('check', ['njk', 'comb'], function(){
-  return gulp.src([
-    DEST + '/**/*.html'
-  ])
-    .pipe(p.using())
-    .pipe(plumberNotify())
-    .pipe(p.w3cjs())
-    .pipe(through2.obj(function(file, enc, cb){
-        cb(null, file);
-        if (!file.w3cjs.success){
-          file.pipe(p.notify('HTML validation error(s) found'));
-        }
-    }))
-});
 
 
 // -------------------------------------------------------------------
@@ -91,27 +56,6 @@ gulp.task('sass', function () {
 });
 
 
-
-// -------------------------------------------------------------------
-// JS
-// -------------------------------------------------------------------
-//JSチェック
-gulp.task('eslint', function(){
-  return gulp.src([
-    SRC + '/**/*.js',
-    '!src/**/jquery.*.js',
-    '!src/**/*.min.js'
-  ])
-  .pipe(p.cached())
-  .pipe(p.using())
-  .pipe(plumberNotify())
-  .pipe(p.eslint({useEslintrc: true}))
-  .pipe(p.eslint.format())
-  .pipe(p.eslint.failAfterError())
-  .pipe(gulp.dest(DEST));
-});
-
-
 //clean
 gulp.task('clean', function (cb) {
   rimraf(DEST, cb);
@@ -121,10 +65,7 @@ gulp.task('clean', function (cb) {
 //copy
 gulp.task('copy', function(){
   return gulp.src([
-    SRC + '/**/*',
-    '!src/**/*.scss',
-    '!src/**/*.njk',
-    '!src/_inc'
+    SRC + '/**/*.html'
   ])
   .pipe(gulp.dest(DEST))
 });
@@ -150,12 +91,9 @@ gulp.task('watch', ['server'], function(){
   gulp.watch([
     SRC + '/**/*',
     '!src/**/*.scss',
-    '!src/**/*.njk',
-    '!src/_inc'
   ], ['copy', browserSync.reload]);
+  gulp.watch([SRC + '/**/*.html'], ['copy', browserSync.reload]);
   gulp.watch([SRC + '/**/*.scss'], ['sass', browserSync.reload]);
-  gulp.watch([SRC + '/**/*.js'], ['eslint', browserSync.reload]);
-  gulp.watch([SRC + '/**/*.njk'], ['njk', browserSync.reload]);
 });
 
 
@@ -163,51 +101,9 @@ gulp.task('default', function (cb) {
   runSequence(
     'clean',
     'copy',
-    ['sass', 'njk'],
+    'sass',
     'watch',
     cb
   );
-});
-
-
-
-// -------------------------------------------------------------------
-// スタイルガイド
-// -------------------------------------------------------------------
-
-//スタイルガイド用sassのコンパイル
-gulp.task('aigis-sass', function () {
-  return gulp.src([
-    GUIDE + '/**/*.scss'
-  ])
-  // .pipe(p.cached())
-  .pipe(p.using())
-  .pipe(plumberNotify())
-  .pipe(p.csscomb())
-  .pipe(p.sass({outputStyle: 'expanded'}).on('error', p.sass.logError))
-  .pipe(gulp.dest(GUIDE))
-});
-
-//aigisの実行
-gulp.task('aigis-compile', ['aigis-sass'], function(){
-  return gulp.src(GUIDE + '/aigis_config.yml')
-    .pipe(p.using())
-    .pipe(plumberNotify())
-    .pipe(p.aigis())
-    .pipe(gulp.dest(GUIDE))
-});
-
-//aigis実行、CSSのasset文字列置換
-gulp.task('aigis', ['aigis-compile'], function(){
-  return gulp.src(GUIDE + '/styleguide/asset/css/style.css')
-    .pipe(p.using())
-    .pipe(plumberNotify())
-    .pipe(p.replace('/asset', '../../asset'))
-    .pipe(gulp.dest(GUIDE + '/styleguide/asset/css/'))
-});
-
-//スタイルガイドの生成
-gulp.task('style', ['aigis'], function(){
-  gulp.watch([GUIDE + '/**/*.scss'], ['aigis']);
 });
 
